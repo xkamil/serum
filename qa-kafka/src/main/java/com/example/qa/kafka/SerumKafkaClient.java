ef253ecb1ef9dc0003fffb1646fe264457215037
kamil <xkamilwrobel@gmail.com>
2022-04-23 14:12:54 +0200

    Add tests

--- qa-kafka/src/main/java/com/example/qa/kafka/SerumKafkaClient.java
+++ qa-kafka/src/main/java/com/example/qa/kafka/SerumKafkaClient.java
@@ -9,10 +9,11 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.ConsumerGroupListing;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.ConsumerGroupState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,13 +25,17 @@ public final class SerumKafkaClient {
       .newCachedThreadPool(new ThreadFactoryBuilder().setDaemon(true).build());
 
   private final Map<String, SerumKafkaConsumer<?>> consumers = new HashMap<>();
+  private KafkaProducer<String, String> producer;
   private final String consumerGroupPrefix;
   private final Properties consumerProperties;
   private final Properties adminProperties;
+  private final Properties producerProperties;
 
-  private SerumKafkaClient(String bootstrapServers, Properties consumerProperties, Properties adminProperties) {
+  private SerumKafkaClient(String bootstrapServers, Properties consumerProperties, Properties adminProperties,
+      Properties producerProperties) {
     this.consumerProperties = consumerProperties;
     this.adminProperties = adminProperties;
+    this.producerProperties = producerProperties;
     this.consumerGroupPrefix = "test-";
   }
 
@@ -60,30 +65,24 @@ public final class SerumKafkaClient {
     deleteEmptyConsumerGroups();
   }
 
-  public static class Builder {
-
-    private final Properties consumerProperties;
-    private final Properties adminProperties;
-    private final String bootstrapServers;
-
-    public Builder(String bootstrapServers) {
-      this.consumerProperties = getDefaultConsumerProperties(bootstrapServers);
-      this.adminProperties = getDefaultAdminProperties(bootstrapServers);
-      this.bootstrapServers = bootstrapServers;
+  public void closeProducer() {
+    if (producer != null) {
+      producer.close();
+      producer = null;
     }
+  }
 
-    public Builder withConsumerProperty(String key, String value) {
-      this.consumerProperties.setProperty(key, value);
-      return this;
-    }
+  public RecordMetadata publishMessage(String topic, String value) {
+    var producerRecord = new ProducerRecord<String, String>(topic, value);
+    return publishMessage(producerRecord);
+  }
 
-    public Builder withAdminProperty(String key, String value) {
-      this.adminProperties.setProperty(key, value);
-      return this;
-    }
-
-    public SerumKafkaClient build() {
-      return new SerumKafkaClient(bootstrapServers, consumerProperties, adminProperties);
+  public RecordMetadata publishMessage(ProducerRecord<String, String> producerRecord) {
+    log.info("Published message on topic {}. Message: {}", producerRecord.topic(), producerRecord.value());
+    try {
+      return getProducer().send(producerRecord).get();
+    } catch (InterruptedException | ExecutionException e) {
+      throw new KafkaClientException("Error when publishing message to topic " + producerRecord.topic(), e);
     }
   }
 
@@ -104,10 +103,46 @@ public final class SerumKafkaClient {
 
   }
 
+  private synchronized KafkaProducer<String, String> getProducer() {
+    if (producer == null) {
+      producer = new KafkaProducer<>(producerProperties);
+    }
+    return producer;
+  }
+
+  public static class Builder {
+
+    private final Properties consumerProperties;
+    private final Properties adminProperties;
+    private final Properties producerProperties;
+    private final String bootstrapServers;
+
+    public Builder(String bootstrapServers) {
+      this.consumerProperties = getDefaultConsumerProperties(bootstrapServers);
+      this.adminProperties = getDefaultAdminProperties(bootstrapServers);
+      this.producerProperties = getDefaultProducerProperties(bootstrapServers);
+      this.bootstrapServers = bootstrapServers;
+    }
+
+    public Builder withConsumerProperty(String key, String value) {
+      this.consumerProperties.setProperty(key, value);
+      return this;
+    }
+
+    public Builder withAdminProperty(String key, String value) {
+      this.adminProperties.setProperty(key, value);
+      return this;
+    }
+
+    public SerumKafkaClient build() {
+      return new SerumKafkaClient(bootstrapServers, consumerProperties, adminProperties, producerProperties);
+    }
+  }
+
   private static Properties getDefaultConsumerProperties(String bootstrapServers) {
     var props = new Properties();
-    props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-    props.put(CommonClientConfigs.SESSION_TIMEOUT_MS_CONFIG, "30000");
+    props.put("bootstrap.servers", bootstrapServers);
+    props.put("session.timeout.ms", "30000");
     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     props.put("auto.offset.reset", "earliest");
@@ -116,9 +151,22 @@ public final class SerumKafkaClient {
 
   private static Properties getDefaultAdminProperties(String bootstrapServers) {
     var props = new Properties();
-    props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-    props.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, "50000");
-    props.put(AdminClientConfig.DEFAULT_API_TIMEOUT_MS_CONFIG, "50000");
+    props.put("bootstrap.servers", bootstrapServers);
+    props.put("request.timeout.ms", "50000");
+    props.put("default.api.timeout.ms", "50000");
+    return props;
+  }
+
+  private static Properties getDefaultProducerProperties(String bootstrapServers) {
+    var props = new Properties();
+    props.put("bootstrap.servers", bootstrapServers);
+    props.put("acks", "all");
+    props.put("retries", 1);
+    props.put("batch.size", 16384);
+    props.put("linger.ms", 1);
+    props.put("buffer.memory", 33554432);
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     return props;
   }
 
---
+++ qa-kafka/src/test/java/com/example/qa/kafka/SerumKafkaClientTest.java
@@ -0,0 +1,103 @@
+package com.example.qa.kafka;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.example.serum.commons.json.JsonEntity;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.stream.Collectors;
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.awaitility.core.ConditionTimeoutException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+
+public class SerumKafkaClientTest {
+
+  public static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"));
+  public static SerumKafkaClient kafkaClient;
+  private static final String TOPIC_1 = "TOPIC_1";
+
+  @BeforeAll
+  static void beforeAll() throws Exception {
+    kafka.start();
+    var bootstrapServers = kafka.getBootstrapServers();
+    kafkaClient = SerumKafkaClient.builder(bootstrapServers).build();
+    createTopics(bootstrapServers, TOPIC_1);
+  }
+
+  @AfterAll
+  static void afterAll() throws Exception {
+    kafkaClient.closeProducer();
+    kafkaClient.unsubscribeAllTopics();
+  }
+
+  @Test
+  void test() {
+    // given message published before client subscribe topic
+    var message1 = "{'type':'type1','number':1}".replace("'", "\"");
+    kafkaClient.publishMessage(TOPIC_1, message1);
+
+    // when client subscribe to topic
+    var event1Consumer = kafkaClient.subscribe(Event1.class);
+    var event2Consumer = kafkaClient.subscribe(Event2.class);
+
+    // and messages are published
+    var message2 = "{'type':'type2','number':2}".replace("'", "\"");
+    var message3 = "{'owner':'Dog'}".replace("'", "\"");
+
+    kafkaClient.publishMessage(TOPIC_1, message2);
+    kafkaClient.publishMessage(TOPIC_1, message3);
+
+    // then message published before subscription should not be found
+    assertThrows(ConditionTimeoutException.class, () -> {
+      event1Consumer.waitForEvent(5, f -> f.getContent().type.equals("type1"));
+    });
+
+    // and messages published after subscription should be found
+    var event2 = event1Consumer.waitForEvent(5, f -> true);
+    var event3 = event2Consumer.waitForEvent(5, f -> true);
+
+    assertAll(
+        () -> assertThat(event2.getContent().type).isEqualTo("type2"),
+        () -> assertThat(event2.getContent().number).isEqualTo(2),
+        () -> assertThat(event3.getContent().owner).isEqualTo("Dog")
+    );
+
+  }
+
+
+  @KafkaEvent(topic = TOPIC_1, filter = "type")
+  public static class Event1 extends JsonEntity {
+
+    public String type;
+    public Integer number;
+  }
+
+  @KafkaEvent(topic = TOPIC_1, filter = "owner")
+  public static class Event2 extends JsonEntity {
+
+    public String owner;
+  }
+
+  private static void createTopics(String bootstrapServers, String... topicNames) throws Exception {
+    var props = new Properties();
+    props.put("bootstrap.servers", kafka.getBootstrapServers());
+
+    try (Admin admin = Admin.create(props)) {
+      int partitions = 5;
+      short replicationFactor = 1;
+      var topics = Arrays.stream(topicNames)
+          .map(topicName -> new NewTopic(topicName, partitions, replicationFactor))
+          .collect(Collectors.toList());
+      var result = admin.createTopics(topics);
+      result.all().get();
+    }
+  }
+}
