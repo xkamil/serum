ff0007809ff705682b1432ae632b6cbb1113bde8
kamil.wrobel <kamil.wrobel@zen.com>
2022-05-06 10:47:54 +0200

    WIP

--- reporting/src/main/java/pl/net/testit/serum/reporting/extension/TestReportAppender.java
+++ reporting/src/main/java/pl/net/testit/serum/reporting/extension/TestReportAppender.java
@@ -5,14 +5,24 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.OutputStreamAppender;
+import io.qameta.allure.Allure;
 import io.qameta.allure.AllureLifecycle;
 import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import pl.net.testit.serum.reporting.BaseTestSuite;
 
 public class TestReportAppender extends OutputStreamAppender<ILoggingEvent> {
 
+  // skip adding logs for those loggers to allure report.
+  private static final List<String> SKIPP_LOGGERS = List.of(
+      "IssuesListExtension",
+      "LoggingExtensions",
+      BaseTestSuite.class.getSimpleName(),
+      AllureLifecycle.class.getSimpleName()
+  );
   private final Level logLevel = Level.INFO;
   private final LogRepository logRepository = LogRepository.getInstance();
   private final LoggerContext loggerContext = new LoggerContext();
@@ -25,24 +35,32 @@ public class TestReportAppender extends OutputStreamAppender<ILoggingEvent> {
 
   @Override
   protected void append(ILoggingEvent event) {
-    if (event.getLevel().isGreaterOrEqual(logLevel)) {
-      String logMessage = formatMessage(event);
-      String loggerName = event.getLoggerName();
+    var loggerName = event.getLoggerName();
+    var logMessage = formatMessage(event);
 
-      if (loggerName.contains("RequestLoggingFilter")) {
-        var header = StringUtils.substringBetween(logMessage, "--request", "\\").trim();
-        logRepository.addLog(header, logMessage);
-
-      } else if (loggerName.contains("ResponseLoggingFilter")) {
-        var header = "HTTP" + StringUtils.substringBetween(logMessage, "HTTP", "\n").trim();
-        logRepository.addLog(header, logMessage);
-
-      } else if (!(loggerName.contains("IssuesListExtension") || loggerName.contains("LoggingExtensions")
-          || loggerName.contains(BaseTestSuite.class.getSimpleName()) || loggerName
-          .contains(AllureLifecycle.class.getSimpleName()))) {
-        logRepository.addLog("logs", logMessage);
-      }
+    if (!event.getLevel().isGreaterOrEqual(logLevel)) {
+      return;
     }
+
+    if (SKIPP_LOGGERS.stream().anyMatch(loggerName::contains)) {
+      return;
+    }
+
+    if (loggerName.contains("RequestLoggingFilter")) {
+      var header = "HTTP" + StringUtils.substringBetween(logMessage, "HTTP", "\n").trim();
+      Allure.getLifecycle().addAttachment(header, "text/plain", "txt", logMessage.getBytes(StandardCharsets.UTF_8));
+      logRepository.addLog("logs", header);
+      return;
+    }
+
+    if (loggerName.contains("ResponseLoggingFilter")) {
+      var header = "HTTP" + StringUtils.substringBetween(logMessage, "HTTP", "\n").trim();
+      Allure.getLifecycle().addAttachment(header, "text/plain", "txt", logMessage.getBytes(StandardCharsets.UTF_8));
+      logRepository.addLog("logs", header);
+      return;
+    }
+
+    logRepository.addLog("logs", logMessage);
   }
 
   private PatternLayoutEncoder getDefaultEncoder() {
@@ -57,4 +75,5 @@ public class TestReportAppender extends OutputStreamAppender<ILoggingEvent> {
     return String.format("%s %s %s - %s", Instant.ofEpochMilli(event.getTimeStamp()), event.getLevel(),
         event.getLoggerName().replaceAll("\\B\\w+(\\.[a-zA-Z])", "$1"), event.getFormattedMessage());
   }
+
 }
